6c1291518aaa8f6f70d5489ada5724e41b37b78a
jeasonyoung <jeason1914@qq.com>
2015-01-17 17:04:16 +0800

    1.修改代理情况下获取真是IP的方法 2.限制系统初始化的IP地址

--- src/main/java/com/examw/netplatform/controllers/admin/HelperController.java
+++ src/main/java/com/examw/netplatform/controllers/admin/HelperController.java
@@ -3,8 +3,10 @@ package com.examw.netplatform.controllers.admin;
 import java.util.UUID;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -46,5 +48,25 @@ public class HelperController {
 	@ResponseBody
 	public String[] randomCode(Integer length){
 		return new String[] { this.agencyUserService.loadRandomCode(length) };
+	}
+	/**
+	 * 获取客户端IP地址。
+	 * @param request
+	 * 客户端请求对象。
+	 * @return 客户端IP地址。
+	 */
+	public static String getRemoteAddr(HttpServletRequest request){
+		if(request == null) throw new IllegalArgumentException("request");
+		String client =  request.getHeader("x-forwarded-for");
+		if(StringUtils.isEmpty(client) || "unknown".equalsIgnoreCase(client)){
+			client =  request.getHeader("Proxy-Client-IP");
+		}
+		if(StringUtils.isEmpty(client) || "unknown".equalsIgnoreCase(client)){
+			client  = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if(StringUtils.isEmpty(client) || "unknown".equalsIgnoreCase(client)){
+			client = request.getRemoteAddr();
+		}
+		return client;
 	}
 }
